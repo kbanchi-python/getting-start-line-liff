f888beb90dd58bc6e135f0d39cf7deee7aa84238
kbanchi1111 <kbanchi.1111@gmail.com>
2022-12-14 15:25:53 +0900

    fix for production

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def setting_post():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
